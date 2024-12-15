3b12bc879fed3293a1b2077122dc320d4eb919fa
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-12-15 05:05:12 -0500

    added user agent

--- main.js
+++ main.js
@@ -6,7 +6,10 @@ const createWindow = () => {
     height: 600
   })
 
-  window.loadFile('index.html')
+  window.loadURL('https://web.whatsapp.com/', {
+    userAgent:
+      'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0'
+  })
 }
 
 app.whenReady().then(() => {
