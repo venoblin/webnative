3dffa2385d695d267f261f0c7a45a835e0b2db50
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-12-29 21:09:45 -0500

    first commit of files

---
+++ include/webApp.h
@@ -0,0 +1,9 @@
+#ifndef WEB_APP_H
+#define WEB_APP_H
+
+class WebApp {
+  public:
+    WebApp() {};
+};
+
+#endif
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -d "build/" ]; then
-  ./build/whatsappdesktop
+  ./build/webnative
 else
   echo "Error: No build found! Run build.sh first."
 fi
